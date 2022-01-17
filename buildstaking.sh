8aa2b766cca9bc4111e238c0646febd6e140bda4
elitexpro <cryptooxelite@gmail.com>
2022-01-17 17:59:22 +0000

    d

--- buildstaking.sh
+++ buildstaking.sh
@@ -1,18 +1,127 @@
-#!/bin/sh
+#!/bin/bash
 
 #Build Flag
-#REBUILD=$1
-#Build optimized result
-#if $REBUILD eq 'TRUE' THEN
-docker run --rm -v "$(pwd)":/code \
---mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
---mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-cosmwasm/rust-optimizer:0.12.4
-#fi
-
-#Upload, Instantiate, Query staking contract
+PARAM=$1
+####################################    Constants    ##################################################
+#depends on mainnet or testnet
 NODE="--node https://rpc.juno.giansalex.dev:443"
 CHAIN_ID=uni-1
-TXFLAG=" $NODE --chain-id $CHAIN_ID --gas-prices 0.03ujunox --gas auto --gas-adjustment 1.3"
+DENOM="ujunox"
 
-junod tx wasm store artifacts/cw20_staking.wasm --from workshop $TXFLAG -y --output json
+#not depends
+NODECHAIN=" $NODE --chain-id $CHAIN_ID"
+TXFLAG=" $NODECHAIN --gas-prices 0.03$DENOM --gas auto --gas-adjustment 1.3"
+WALLET="--from workshop"
+WASMFILE="artifacts/cw20_staking.wasm"
+TXHASHFILE="uploadtx"
+CONTRACTADDRFILE="contractaddr"
+WORKSHOPADDR="juno1htjut8n7jv736dhuqnad5mcydk6tf4ydeaan4s"
+####################################    Functions    ###################################################
+
+OptimizeBuild() {
+
+    echo "================================================="
+    echo "Optimize Build Start"
+    docker run --rm -v "$(pwd)":/code \
+        --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+        --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+        cosmwasm/rust-optimizer:0.12.4
+    echo "================================================="
+    
+}
+
+Upload() {
+    echo "================================================="
+    echo "Upload $WASMFILE"
+    UPLOADTX=$(junod tx wasm store $WASMFILE $WALLET $TXFLAG --output json -y | jq -r '.txhash')
+    echo $UPLOADTX
+    #save to file
+    echo $UPLOADTX > $TXHASHFILE
+    echo "writed last transaction hash to $TXHASHFILE"
+    echo "================================================="
+    
+}
+
+GetCode() {
+    echo "================================================="
+    echo "Get code from txHash written on $TXHASHFILE"
+    #read from file
+    TXHASH=$(cat $TXHASHFILE)
+    echo "read last transaction hash from $TXHASHFILE"
+    echo $TXHASH
+    QUERYTX="junod query tx $TXHASH $NODECHAIN --output json"
+	CODE_ID=$($QUERYTX | jq -r '.logs[0].events[-1].attributes[0].value')
+	echo $CODE_ID
+    echo "================================================="
+    return $CODE_ID
+}
+
+Instantiate() {
+    GetCode
+    CODE_ID=$?
+    junod tx wasm instantiate $CODE_ID \
+    '{"name":"CREWStaking","symbol":"POOD","decimals":6,"initial_balances":[{"address":"$WORKSHOPADDR","amount":"12345678000"}]}' \
+    --amount 50000$DENOM --label "Poodlecoin erc20" $WALLET $TXFLAG -y
+
+}
+
+GetContractAddress() {
+    echo "================================================="
+    echo "Get contract address by code"
+    GetCode
+    CODE_ID=$?
+    #QUERYCONTRACT="junod query wasm list-contract-by-code $CODE_ID $NODECHAIN --output json"
+    #echo $QUERYCONTRACT
+    CONTRACT_ADDR=$(junod query wasm list-contract-by-code $CODE_ID $NODECHAIN --output json | jq -r '.contracts[0]')
+    echo $CONTRACT_ADDR
+    echo "================================================="
+}
+
+GetBalance() {
+    GetContractAddress
+    junod query bank balances $? $NODECHAIN
+}
+
+GetContract() {
+    GetContractAddress
+    junod query wasm contract $? $NODECHAIN
+}
+
+ListCode() {
+    echo "================================================="
+    echo "Get List of Codes"
+    junod query wasm list-code $NODECHAIN --output json
+    echo "================================================="
+}
+
+
+
+#################################### End of Function ###################################################
+# Upload
+# QueryTX $?
+# echo $? > lasthash
+$PARAM
+# if [[ $PARAM == "build" ]]; then
+#     OptimizeBuild
+# fi
+
+# if [[ $PARAM == "upload" ]]; then
+#     Upload
+# fi
+
+# if [[ $PARAM == "getcode" ]]; then
+#     GetCode
+# fi
+
+# if [[ $PARAM == "listcode" ]]; then
+#     ListCode
+# fi
+
+# if [[ $PARAM == "contractaddr" ]]; then
+#     GetContractAddress
+# fi
+
+# if [[ $PARAM == "instantiate" ]]; then
+#     Instantiate
+# fi
+
