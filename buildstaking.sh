7d41fba8cd7a679742c1cbd3b0d46590f61319c2
elitexpro <cryptooxelite@gmail.com>
2022-01-17 23:49:57 +0900

    dd

--- buildstaking.sh
+++ buildstaking.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 #Build Flag
-REBUILD=$1
+#REBUILD=$1
 #Build optimized result
-if $REBUILD eq 'TRUE' THEN
-  docker run --rm -v "$(pwd)":/code \
-    --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
-    --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-    cosmwasm/rust-optimizer:0.12.4
-fi
+#if $REBUILD eq 'TRUE' THEN
+docker run --rm -v "$(pwd)":/code \
+--mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+--mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+cosmwasm/rust-optimizer:0.12.4
+#fi
 
 #Upload, Instantiate, Query staking contract
 NODE="--node https://rpc.juno.giansalex.dev:443"
